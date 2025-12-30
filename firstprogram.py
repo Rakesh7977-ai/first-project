1374505e68b8b99e958c460c960c43fc0f872417
Rakesh7977-ai <reebo2999@gmail.com>
2025-12-30 10:31:31 +0000

    new work

--- firstprogram.py
+++ firstprogram.py
@@ -1,6 +1,7 @@
 name = "Rakesh chauhan"
 age = 16
 passion = "student"
+price = 19.99
 
 print("my name is:",name)
 print("my age is:",age)
@@ -9,4 +10,52 @@ print("my passion is:",passion)
 print(type(name))
 print(type(age))
 print(type(passion))
+print(type(price))
+
+age = 16
+old = False
+a = None
+print(type(old))
+print(type(a))
+
+# Arithmetic operators
+
+# a = 10
+# b = 25
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)   # Remainder
+# print(a ** b)  #a^b
+
+
+# Relation / comparison operators
+
+# a = 50
+# b = 20 
+
+# print(a == b)
+# print(a != b)
+# print(a <= b)
+# print(a > b)
+
+
+# Assignment operators
+
+num = 10
+num = num + 20   #10+20=30
+print(num)
+print("num :",num)
+
+
+
+
+
+
+
+
+
+
 
