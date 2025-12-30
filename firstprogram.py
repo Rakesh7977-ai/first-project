8f18f07ddfeae00e68a87c8765e5004af6528295
Rakesh7977-ai <reebo2999@gmail.com>
2025-12-30 09:25:40 +0000

    new python file

---
+++ firstprogram.py
@@ -0,0 +1,12 @@
+name = "Rakesh chauhan"
+age = 16
+passion = "student"
+
+print("my name is:",name)
+print("my age is:",age)
+print("my passion is:",passion)
+
+print(type(name))
+print(type(age))
+print(type(passion))
+
