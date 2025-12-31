0284cce876b6aa10b14e616c70b31b1c5ee10171
Rakesh7977-ai <reebo2999@gmail.com>
2025-12-31 09:59:37 +0000

    final modification in firstprogramm

--- firstprogram.py
+++ firstprogram.py
@@ -1,64 +1,114 @@
-name = "Rakesh chauhan"
-age = 16
-passion = "student"
-price = 19.99
+# # name = "Rakesh chauhan"
+# # age = 16
+# # passion = "student"
+# # price = 19.99
 
-print("my name is:",name)
-print("my age is:",age)
-print("my passion is:",passion)
+# # print("my name is:",name)
+# # print("my age is:",age)
+# # print("my passion is:",passion)
 
-print(type(name))
-print(type(age))
-print(type(passion))
-print(type(price))
+# # print(type(name))
+# # print(type(age))
+# # print(type(passion))
+# # print(type(price))
 
-age = 16
-old = False
-a = None
-print(type(old))
-print(type(a))
+# # age = 16
+# # old = False
+# # a = None
+# # print(type(old))
+# # print(type(a))
 
-# Arithmetic operators
+# # Arithmetic operators
 
-# a = 10
-# b = 25
+# # a = 10
+# # b = 25
 
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a % b)   # Remainder
-# print(a ** b)  #a^b
-
-
-# Relation / comparison operators
-
-# a = 50
-# b = 20 
-
-# print(a == b)
-# print(a != b)
-# print(a <= b)
-# print(a > b)
+# # print(a + b)
+# # print(a - b)
+# # print(a * b)
+# # print(a / b)
+# # print(a % b)   # Remainder
+# # print(a ** b)  #a^b
 
 
-# Assignment operators
+# # Relation / comparison operators
 
-num = 10
-num = num + 20   #10+20=30
-print(num)
-print("num :",num)
+# # a = 50
+# # b = 20 
 
-
-
-
-
+# # print(a == b)
+# # print(a != b)
+# # print(a <= b)
+# # print(a > b)
 
 
+# # Assignment operators
+
+# # num = 10
+# # num = num + 20   #10+20=30
+# # print(num)
+# # print("num :",num
+
+# # logical operator
+
+# # a = 50
+# # b = 30
+
+# # print(not False)
+# # print(not (a > b))
+
+# # val1 = True                         # operator ans is true when both value are true
+# # val2 = False                                
+# # print("and operator:", val1 and val2)
+
+# # print("or operator:", val1 or val2)
+
+# # print("OR operator:", (a == b) or (a > b))
+
+
+#         # type conversion
+
+# # a = 2
+# # b = 4.25
+
+# # sum = a + b     #2.0 + 4.25 =>6.25
+# # print(sum)
+
+# # a = float("20")
+# # b = 5.99
+
+# # print(type(a))
+# # print(a + b)
+
+#     # input function
+#     # input function allow you to print output on input
+
+# name = input("enter your name :")
+# print("welcome" , name)
+
+# val = int(input("enter your val:"))         #we can use casting function like int, float, str to change the type of val
+# print(type(val), val)
 
 
 
+name = input("enter your name :")
+age = int(input("enter your age: "))
+marks = int(input("enter your marks: "))
+
+print("Welcome", name)
+print("your age", age)
+print("your result", marks)
 
 
 
+val1 = int(input("enter val1: "))
+val2 = int(input("enter val2: "))
+
+print("sum: ", val1+val2)
+
+
+a = float(input("enter first: "))
+b = float(input("enter second: "))
+
+print("the average is=", (a+b)/2)
 
