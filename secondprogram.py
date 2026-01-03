3c753f37b881915ab994177fba9ca038e1c187e0
Rakesh7977-ai <reebo2999@gmail.com>
2026-01-03 09:57:22 +0000

    new work

---
+++ secondprogram.py
@@ -0,0 +1,47 @@
+
+        #strings and conditions
+
+str1 = "Rakesh"
+str2 = "Chauhan"
+final_str = str1 + " " + str2
+print(final_str)
+
+len1 = len(str1)
+len2 = len(str2)
+final_str2 = len1+len2
+print(len1)
+print(len2)
+print(final_str2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
