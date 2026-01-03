1beadc6f3ff1dcdc6288f4efc82dac268796f3ac
Rakesh7977-ai <reebo2999@gmail.com>
2026-01-03 11:41:30 +0000

    finl update second programm

--- secondprogram.py
+++ secondprogram.py
@@ -1,44 +1,112 @@
 
         #strings and conditions
 
-str1 = "Rakesh"
-str2 = "Chauhan"
-final_str = str1 + " " + str2
-print(final_str)
+# str1 = "Rakesh"
+# str2 = "Chauhan"
+# final_str = str1 + " " + str2
+# print(final_str)
 
-len1 = len(str1)
-len2 = len(str2)
-final_str2 = len1+len2
-print(len1)
-print(len2)
-print(final_str2)
-
-
+# len1 = len(str1)
+# len2 = len(str2)
+# final_str2 = len1+len2
+# print(len1)
+# print(len2)
+# print(final_str2)
 
 
+        # indexing starts with whole number in python
+
+# str = "Rakesh chauhan"
+# print(str[5])
+
+        #Sliceing in python for ML learning
+
+# str = "Rakesh chavan"
+# print(str[0:6])
+
+# print(str[7:15])
+# print(str[0:7])
+
+
+# str2 = "apple"
+# print(str2[-8:-2])
+
+        #str function
+
+# str = "i am a hacker"
+# print(str.endswith("er"))
+# str = str.capitalize()
+# print(str.capitalize())
+# print(str)
+
+# print(str.find("h"))
+# print(str.find("a"))
+# print(str.replace("a","o"))
+# print(str.count("a"))
+
+# name = "Rakesh"
+# a = input("enter your name :")
+# print("my name is:", len(name))
+
+# str = "heei$bbwhb4SS$WUSH$"
+# print(str.find("$"))
+
+        #Conditional statement
+
+# age = 15
+# if(age >= 18):
+#     print("universal rights")
+
+# else:
+#     print("not having universal rights")
+
+
+
+# light = "yellow"
+
+# if(light == "red"):
+#     print("stop")
+
+# elif(light == "green"):
+#     print("go")
+
+# elif(light == "yellow"):
+#     print("go slow")
+
+# else:
+#     print('light is broken')
 
 
 
 
+# marks = int(input(" enter student marks : "))
+
+# if(marks >= 90):
+#     print("A grade")
+
+# elif(marks >= 80 and marks < 90):
+#     print("B grade")
 
 
+# elif(marks >= 70 and marks < 80):
+#     print("C grade")
+
+# elif(marks >= 33 and marks < 70):
+#     print("D grade")
 
 
+        #nesting
 
+age2 = 76
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if(age2 >= 18):
+    if(age2 >= 80):
+        print("cannot drive")
+    else:
+        print("can drive")
+    
+else:
+    print("not eligable")
 
 
 
